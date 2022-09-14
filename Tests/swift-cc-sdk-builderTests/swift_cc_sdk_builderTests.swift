1aab41914370cd576109501aff64f644c326293e
Max Desiatov <m_desiatov@apple.com>
2022-09-14 10:59:34 +0100

    Add empty SwiftPM project

---
+++ Tests/swift-cc-sdk-builderTests/swift_cc_sdk_builderTests.swift
@@ -0,0 +1,23 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift open source project
+//
+// Copyright (c) 2022 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+import XCTest
+@testable import swift_cc_sdk_builder
+
+final class swift_cc_sdk_builderTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(swift_cc_sdk_builder().text, "Hello, World!")
+    }
+}
