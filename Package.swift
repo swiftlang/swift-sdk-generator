71917b922998f49697e0e3c36f7a404bcc047de1
Max Desiatov <m_desiatov@apple.com>
2023-02-27 14:35:01 +0000

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -15,10 +15,10 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.13.2"),
+        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.15.0"),
         .package(url: "https://github.com/apple/swift-system", from: "1.2.1"),
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-async-algorithms.git", from: "0.0.4")
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.2"),
+        .package(url: "https://github.com/apple/swift-async-algorithms.git", from: "0.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
