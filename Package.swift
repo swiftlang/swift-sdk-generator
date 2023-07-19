ff0c65c42eba773c4e7c82c022ce52ecc1353e08
Max Desiatov <m_desiatov@apple.com>
2023-07-19 16:38:37 +0100

    Update minor versions in `Package.swift`

--- Package.swift
+++ Package.swift
@@ -15,13 +15,13 @@ let package = Package(
   ],
   dependencies: [
     // Dependencies declare other packages that this package depends on.
-    .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.17.0"),
+    .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.18.0"),
     .package(url: "https://github.com/apple/swift-system", from: "1.2.1"),
     .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.2"),
     .package(url: "https://github.com/apple/swift-async-algorithms.git", from: "0.1.0"),
     .package(url: "https://github.com/apple/swift-atomics.git", from: "1.0.0"),
     .package(url: "https://github.com/apple/swift-nio.git", from: "2.56.0"),
-    .package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.17.0"),
+    .package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.19.0"),
     .package(url: "https://github.com/apple/swift-log.git", from: "1.5.2"),
     .package(url: "https://github.com/apple/swift-collections.git", from: "1.0.0"),
   ],
@@ -38,6 +38,7 @@ let package = Package(
     .target(
       name: "SwiftSDKGenerator",
       dependencies: [
+        .target(name: "AsyncProcess"),
         .product(name: "AsyncAlgorithms", package: "swift-async-algorithms"),
         .product(name: "AsyncHTTPClient", package: "async-http-client"),
         .product(name: "SystemPackage", package: "swift-system"),
