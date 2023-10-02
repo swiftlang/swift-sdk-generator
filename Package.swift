f20682bd7d7510650bdfa7605a5d4ef177aab2d0
Max Desiatov <m_desiatov@apple.com>
2023-10-02 10:45:18 +0100

    Bump AHC to 0.19.0

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
   ],
   dependencies: [
     // Dependencies declare other packages that this package depends on.
-    .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.18.0"),
+    .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.19.0"),
     .package(url: "https://github.com/apple/swift-system", from: "1.2.1"),
     .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.2"),
     .package(url: "https://github.com/apple/swift-async-algorithms.git", exact: "1.0.0-alpha"),
