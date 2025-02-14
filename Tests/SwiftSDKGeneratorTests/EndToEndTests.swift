ff9e63bc0e5989419d1247bd20f4423508d93c63
Euan Harris <euanh@apple.com>
2025-02-14 09:11:07 +0000

    wip: Force no host toolchain to avoid downloads

--- Tests/SwiftSDKGeneratorTests/EndToEndTests.swift
+++ Tests/SwiftSDKGeneratorTests/EndToEndTests.swift
@@ -124,7 +124,7 @@ final class RepeatedBuildTests: XCTestCase {
 
     // Test that an existing SDK can be rebuilt without cleaning up.
     // Test with no arguments by default:
-    var possibleArguments = ["--host-toolchain"]
+    var possibleArguments = ["--no-host-toolchain"]
     do {
       try await Shell.run("podman ps")
       possibleArguments.append("--with-docker --linux-distribution-name rhel --linux-distribution-version ubi9")
@@ -176,7 +176,7 @@ struct SDKConfiguration {
   var sdkGeneratorArguments: String {
     return [
       "--sdk-name \(bundleName)",
-      "--host-toolchain",
+      "--no-host-toolchain",
       withDocker ? "--with-docker" : nil,
       "--swift-version \(swiftVersion)-RELEASE",
       testLinuxSwiftSDKs ? "--host \(hostArch!)-unknown-linux-gnu" : nil,
