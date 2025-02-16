5b93bd8ae9268708a653133ba153adc12577584d
Jesse L. Zamora <xtremekforever@gmail.com>
2025-02-16 14:51:44 -0500

    Improve formatting of --linux-distribution-version help string
     - Each distribution and supported versions has a single line.

--- Sources/GeneratorCLI/GeneratorCLI.swift
+++ Sources/GeneratorCLI/GeneratorCLI.swift
@@ -199,9 +199,9 @@ extension GeneratorCLI {
 
     @Option(
       help: """
-      Version of the Linux distribution used as a target platform. Available options for Ubuntu: `20.04`, \
-      `22.04` (default when `--linux-distribution-name` is `ubuntu`), `24.04`. Available options for RHEL: `ubi9` (default when \
-      `--linux-distribution-name` is `rhel`).
+      Version of the Linux distribution used as a target platform.
+      Available options for Ubuntu: `20.04`, `22.04` (default when `--linux-distribution-name` is `ubuntu`), `24.04`.
+      Available options for RHEL: `ubi9` (default when `--linux-distribution-name` is `rhel`).
       """
     )
     var linuxDistributionVersion: String?
