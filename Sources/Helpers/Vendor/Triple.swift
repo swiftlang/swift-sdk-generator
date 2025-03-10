e7323a8906e13bccc3e0a59f1857b387996a35d4
Max Desiatov <m_desiatov@apple.com>
2025-03-10 10:20:41 +0000

    Unify license headers

--- Sources/Helpers/Vendor/Triple.swift
+++ Sources/Helpers/Vendor/Triple.swift
@@ -1,6 +1,6 @@
 //===--------------- Triple.swift - Swift Target Triples ------------------===//
 //
-// This source file is part of the Swift.org open source project
+// This source file is part of the Swift open source project
 //
 // Copyright (c) 2014 - 2019 Apple Inc. and the Swift project authors
 // Licensed under Apache License v2.0 with Runtime Library Exception
